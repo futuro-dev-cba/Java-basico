a07eaaa2e162209d3720a5b11c0d197f6b9ee74a
futuro-dev-cba <victorhsa.comercial@gmail.com>
2023-03-12 18:37:11 -0400

    Tipos e variaveis do Java Basico

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte idade = 99;
+        short ano = 2023;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14f;
+        double salario = 1234.23;
+        String meuNome = "Victor Hugo";
+        final String BR = "Brasil";
+        long resultado = idade * cpf;
+
+
+        System.out.println(resultado);
     }
 }
